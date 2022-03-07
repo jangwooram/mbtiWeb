d1c317cf7d2e00b28ec58e86337931bb50a216d3
wooram <jwr199209@gmail.com>
2022-03-07 19:58:20 +0900

    텍스트 반복문으로 변경중

--- src/component/Content-main.js
+++ src/component/Content-main.js
@@ -3,73 +3,22 @@ import './Content-main.css'
 
 class ContentMain extends Component {
     render() {
+        let mbtiName = ["ENTJ","ENTP","ESFJ","ESTJ","ENFJ","ENFP","ESFP",
+            "ESTP","INTJ","INTP","ISFJ","ISTJ","INFJ","INFP","ISFP","ISTP"]
+        let nameList=[];
+        let i=0;
+        while (i<mbtiName.length){
+            nameList.push(
+                <div className="board">
+                    <div className="icon"/>
+                    <div className="text purple-color">{mbtiName[i]}</div>
+                </div>
+            )
+        }
         return(
             <div className="contents">
                 <div className="mbti-board">
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text purple-color">ENFJ</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text purple-color">ENFP</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text blue-color">ENTJ</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text blue-color">ENTP</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text green-color">ESFJ</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text green-color">ESFP</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text yellow-color">ESTJ</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text yellow-color">ESTP</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text purple-color">INFJ</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text purple-color">INFP</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text blue-color">INTJ</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text blue-color">INTP</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text green-color">ISFJ</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text green-color">ISFP</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text yellow-color">ISTJ</div>
-                    </div>
-                    <div className="board">
-                        <div className="icon"></div>
-                        <div className="text yellow-color">ISTP</div>
-                    </div>
+                    {nameList}
                 </div>
             </div>
         );
