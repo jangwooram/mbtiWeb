dd0f0cafaab7fc8eb54b8881da92fc39cbf37274
wooram <jwr199209@gmail.com>
2022-03-07 20:17:56 +0900

    mbtiName 반복문

--- src/component/Content-main.js
+++ src/component/Content-main.js
@@ -3,20 +3,43 @@ import './Content-main.css'
 
 class ContentMain extends Component {
     render() {
-        let mbtiName = ["ENTJ","ENTP","ESFJ","ESTJ","ENFJ","ENFP","ESFP",
-            "ESTP","INTJ","INTP","ISFJ","ISTJ","INFJ","INFP","ISFP","ISTP"]
-        let nameList=[];
-        let i=0;
-        while (i<mbtiName.length){
-            nameList.push(
-                <div className="board">
-                    <div className="icon"/>
-                    <div className="text purple-color">{mbtiName[i]}</div>
-                </div>
-            )
-            i+=1
+        let mbtiName = ["ENTJ", "ENTP", "ESFJ", "ESTJ", "ENFJ", "ENFP", "ESFP",
+            "ESTP", "INTJ", "INTP", "ISFJ", "ISTJ", "INFJ", "INFP", "ISFP", "ISTP"];
+        let nameList = [];
+        let i = 0;
+        while (i < mbtiName.length) {
+            if (i < 4) {
+                nameList.push(
+                    <div className="board">
+                        <div className="icon"/>
+                        <div className="text purple-color">{mbtiName[i]}</div>
+                    </div>
+                )
+            } else if (i < 8) {
+                nameList.push(
+                    <div className="board">
+                        <div className="icon"/>
+                        <div className="text blue-color">{mbtiName[i]}</div>
+                    </div>
+                )
+            }else if (i< 12) {
+                nameList.push(
+                    <div className="board">
+                        <div className="icon"/>
+                        <div className="text green-color">{mbtiName[i]}</div>
+                    </div>
+                )
+            } else {
+                nameList.push(
+                    <div className="board">
+                        <div className="icon"/>
+                        <div className="text yellow-color">{mbtiName[i]}</div>
+                    </div>
+                )
+            }
+            i += 1;
         }
-        return(
+        return (
             <div className="contents">
                 <div className="mbti-board">
                     {nameList}
