cdd8211563099c63dc8b8e850f3d1f68d95fa40c
wooram <jwr199209@gmail.com>
2022-03-08 18:31:20 +0900

    mbti 순서 변경

--- src/component/Content-main.js
+++ src/component/Content-main.js
@@ -3,8 +3,8 @@ import './Content-main.css'
 
 class ContentMain extends Component {
     render() {
-        let mbtiName = ["ENTJ", "ENTP", "ESFJ", "ESTJ", "ENFJ", "ENFP", "ESFP",
-            "ESTP", "INTJ", "INTP", "ISFJ", "ISTJ", "INFJ", "INFP", "ISFP", "ISTP"];
+        let mbtiName = ["INTJ", "INTP", "ENTJ", "ENTP", "INFJ", "INFP", "ENFJ",
+            "ENFP", "ISTJ", "ISFJ", "ESTJ", "ESFJ", "ISTP", "ISFP", "ESTP", "ESFP"];
         let nameList = [];
         let i = 0;
         while (i < mbtiName.length) {
@@ -19,14 +19,14 @@ class ContentMain extends Component {
                 nameList.push(
                     <div className="board">
                         <div className="icon"><img src={process.env.PUBLIC_URL + '/img/icon-'+(i+1)+'.png'} alt=""/></div>
-                        <div className="text blue-color">{mbtiName[i]}</div>
+                        <div className="text green-color">{mbtiName[i]}</div>
                     </div>
                 )
             }else if (i< 12) {
                 nameList.push(
                     <div className="board">
                         <div className="icon"><img src={process.env.PUBLIC_URL + '/img/icon-'+(i+1)+'.png'} alt=""/></div>
-                        <div className="text green-color">{mbtiName[i]}</div>
+                        <div className="text blue-color">{mbtiName[i]}</div>
                     </div>
                 )
             } else {
