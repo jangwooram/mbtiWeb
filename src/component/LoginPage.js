32ff3f2d1a8855bd84c9c20c63d07f9726b0d87f
wooram <jwr199209@gmail.com>
2022-03-08 20:54:08 +0900

    useState 사용하기 - login

---
+++ src/component/LoginPage.js
@@ -0,0 +1,36 @@
+import React,{useState} from "react";
+
+function LoginPage() {
+
+    const submitDate = (event) => {
+        if(event.keyCode === 13) {
+            onSubmit();
+        }
+    }
+
+    const onSubmit = () => {
+        console.log('로그인 정보 ',Username, Password);
+        alert("enter")
+    }
+    const [Username, setUsername] = useState('');
+    const [Password, setPassword] = useState('');
+    return(
+        <div>
+            <form onSubmit={onSubmit} action="">
+                <input
+                    placeholder={"username"}
+                    value={Username}
+                    onChange={(e) => setUsername(e.target.value)}
+                /><br/>
+                <input
+                    placeholder={"Password"}
+                    value={Password}
+                    onChange={(e)=> setPassword(e.target.value)}
+                /><br/>
+                <button type={"submit"}>Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default LoginPage;
--- src/component/Partice-component.js
+++ src/component/Partice-component.js
@@ -1,14 +1,44 @@
 import React, {useState} from "react";
 
 function ParticeComponent() {
+
+    const submitDate = (event) => {
+        if(event.keyCode === 13) {
+            onSubmit();
+        }
+    }
+
+    const onSubmit = () => {
+        console.log('로그인 정보 ',Username, Password);
+        alert("enter")
+    }
+/*  텍스트 변경하기
     const [text, setText] = useState('hello');
     const updateText = () => {
         setText('안녕');
-    }
+    }*/
+
+    const [Username, setUsername] = useState('');
+    const [Password, setPassword] = useState('');
+
     return (
         <div>
-            <span>{text}</span>
-            <button onClick={updateText}>updeate</button>
+            <form onSubmit={onSubmit} action="">
+            <input
+                placeholder={"username"}
+                value={Username}
+                onChange={(e) => setUsername(e.target.value)}
+            /><br/>
+            <input
+                placeholder={"Password"}
+                value={Password}
+                onChange={(e)=> setPassword(e.target.value)}
+            /><br/>
+            <button type={"submit"}>Login</button>
+            </form>
+            {/* 텍스트 변경하기
+             <span>{text}</span>
+            <button onClick={updateText}>updeate</button>*/}
         </div>
     );
 
