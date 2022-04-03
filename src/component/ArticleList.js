82b91364ef4d6a71fe55b2a2f800f04012350e7d
wooram <jwr199209@gmail.com>
2022-04-03 23:58:02 +0900

    list

--- src/component/ArticleList.js
+++ src/component/ArticleList.js
@@ -2,6 +2,7 @@ import React,{useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
 import axios from "axios";
 import instance from "../ApiController";
+import './ArticleList.css';
 
 function ArticleList() {
     const {category} = useParams();
@@ -28,12 +29,14 @@ function ArticleList() {
             <h1>ArticleList</h1>
             {articles.map(article =>(
                 <div key={article.id}>
-                    <h1>{article.category}</h1>
-                    <h1>{article.title}</h1>
-                    <h1>{article.contents}</h1>
-                    <h1>{article.hits}</h1>
-                    <h1>{article.created_at}</h1>
-                    <h1>{article.author}</h1>
+                    <ul>
+                        <li>{article.category}</li>
+                        <li>{article.title}</li>
+                        <li>{article.author}</li>
+                        <li>{article.created_at}</li>
+                    </ul>
+                    {/*<h1>{article.contents}</h1>
+                    <h1>{article.hits}</h1>*/}
                 </div>
             ))}
             
