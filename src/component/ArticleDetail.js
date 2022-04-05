d98e819daf355fe8498b88661bfbd768abebd937
wooram <jwr199209@gmail.com>
2022-04-05 19:55:17 +0900

    ..

--- src/component/ArticleDetail.js
+++ src/component/ArticleDetail.js
@@ -1,13 +1,49 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
+import instance from "../ApiController";
+import {Box} from "@mui/material";
+import Paper from "@mui/material/Paper";
 
+
+function Item() {
+    return null;
+}
 
 const ArticleDetail = () => {
     const {article_id} = useParams();
-    console.log(article_id);
+    const baseurl = process.env.REACT_APP_BASE_URL;
+    const [details, setDetails] = useState([]);
+    console.log('article_id',article_id);
+    useEffect(function (){
+        async function getArticleDetail() {
+            try {
+                const r = await instance.get(baseurl + '/api/v1/articles/detail/' + article_id + '/');
+                setDetails(r.data.data);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        getArticleDetail();
+    },[])
     return (
-        <div>
-
+        <div className="articleDetail">
+            <Box
+                sx={{
+                    width:'100%',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    '& > :not(style)': {
+                        m: 1,
+                        width: '100%',
+                        height: 50,
+                        lineHeight:'50px',
+                    },
+                }}
+            >
+                <Paper elevation={1}>
+                    <div>{details.title}</div>
+                </Paper>
+            </Box>
         </div>
     );
 };
