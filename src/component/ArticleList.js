78333d96347efab907ea770bd84f3872c7e6dbab
wooram <jwr199209@gmail.com>
2022-04-11 12:52:57 +0900

    pagination

--- src/component/ArticleList.js
+++ src/component/ArticleList.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import { useTheme } from '@mui/material/styles';
+import PropTypes, {func} from 'prop-types';
+import {useTheme} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -18,7 +18,7 @@ import LastPageIcon from '@mui/icons-material/LastPage';
 import {useEffect, useState} from "react";
 import instance from "../ApiController";
 import {useParams} from "react-router-dom";
-import {TableHead} from "@mui/material";
+import {Pagination, TableHead} from "@mui/material";
 import {Link} from "@mui/icons-material";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -28,12 +28,13 @@ function ArticleList() {
     const {category} = useParams();
     const baseurl = process.env.REACT_APP_BASE_URL;
     const [articles, setArticles] = useState([]);
-    const [totalCount, setTotalCount] = useState([])
+    const [totalCount, setTotalCount] = useState([]);
+    const [pageNum,setPageNum] = useState(1);
 
-    useEffect(() => {
+    useEffect((pageNum) => {
         async function getArticles() {
             try {
-                const r = await instance.get(baseurl + '/api/v1/articles/list/' + category + '/?offset='+page+'&limit=3');
+                const r = await instance.get(baseurl + '/api/v1/articles/list/' + category + '/?offset=' + pageNum + '&limit=20');
                 //console.log(r.data);
                 setTotalCount(r.data.total_count);
                 setArticles(r.data.data);
@@ -45,25 +46,21 @@ function ArticleList() {
         // console.log('....',articles);
         getArticles();
     }, []);
-    let page= 1;
-    let showList = 3;
-    let totalPage= 1;
-    let EndNum = page * 20;
-    let StartNum = EndNum-19;
-    let offset = StartNum;
-    let pageCount = () => {
-        if(totalCount<3) {
+
+    let showList = 20;
+    let totalPage = 1;
+    let pageCount = function () {
+        if (totalCount < 3) {
             totalPage = 1;
             return;
         }
-        if(totalCount%showList === 0){
-            totalPage = parseInt(totalCount/showList);
+        if (totalCount % showList === 0) {
+            totalPage = parseInt(totalCount / showList);
         }
-        totalPage = parseInt(totalCount/showList+1);
+        totalPage = parseInt(totalCount / showList + 1);
     }
     pageCount();
 
-    console.log(totalPage);
     const columns = [
         {id: '타입', label: '타입', minWidth: "10%"},
         {id: '제목', label: '제목', minWidth: "60%"},
@@ -72,12 +69,24 @@ function ArticleList() {
     ];
 
     function linkClick(id) {
-        window.location.href = '/ArticleDetail/'+id;
+        window.location.href = '/ArticleDetail/' + id;
     }
+    const handleChange = (event, value) => {
+        setPageNum(value);
+        async function getArticles() {
+            try {
+                const r = await instance.get(baseurl + '/api/v1/articles/list/' + category + '/?offset=' + value + '&limit=20');
+                setArticles(r.data.data);
+            } catch (e) {
+                console.log(e);
+            }
 
+        }
+        getArticles();
+    };
     return (
         <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 500}} aria-label="custom pagination table">
+            <Table sx={{minWidth: 500}} aria-label="custom pagination table">
                 <TableHead>
                     <TableRow>
                         {columns.map((column) => (
@@ -89,33 +98,30 @@ function ArticleList() {
                 </TableHead>
                 <TableBody>
                     {articles
-                    .map((article) => (
-                        <TableRow onClick={(e)=>{linkClick(article.id)}} key={article.id} hover={true} style={{cursor: 'pointer'}}>
-                            <TableCell style={{width: '10%'}} component="th" scope="row">
-                                {article.category}
-                            </TableCell>
-                            <TableCell style={{width:'60%'}} component="th" scope="row">
-                                {article.title}
-                            </TableCell>
-                            <TableCell style={{ width: '10%', textAlign: "left" }} align="right">
-                                {article.author}
-                            </TableCell>
-                            <TableCell style={{ width: '15%', textAlign: "left" }} align="right">
-                                {article.created_at}
-                            </TableCell>
-                        </TableRow>
-                    ))}
+                        .map((article) => (
+                            <TableRow onClick={(e) => {
+                                linkClick(article.id)
+                            }} key={article.id} hover={true} style={{cursor: 'pointer'}}>
+                                <TableCell style={{width: '10%'}} component="th" scope="row">
+                                    {article.category}
+                                </TableCell>
+                                <TableCell style={{width: '60%'}} component="th" scope="row">
+                                    {article.title}
+                                </TableCell>
+                                <TableCell style={{width: '10%', textAlign: "left"}} align="right">
+                                    {article.author}
+                                </TableCell>
+                                <TableCell style={{width: '15%', textAlign: "left"}} align="right">
+                                    {article.created_at}
+                                </TableCell>
+                            </TableRow>
+                        ))}
                 </TableBody>
                 <TableFooter>
                     <TableRow>
-                        <TableCell colSpan={4} style={{textAlign: "center", margin: 'auto' }}>
-                            <IconButton>
-                                <ArrowBackIosIcon/>
-                            </IconButton>
-
-                            <IconButton>
-                                <ArrowForwardIosIcon/>
-                            </IconButton>
+                        <TableCell colSpan={4} style={{textAlign: "center", margin: 'auto'}}>
+                            <Pagination
+                                count={totalPage} page={pageNum} onChange={handleChange} shape="rounded"/>
                         </TableCell>
                     </TableRow>
                 </TableFooter>
