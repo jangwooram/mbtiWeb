3430493bb7116ee800e096fd1f4d7175e58f8e92
wooram <jwr199209@gmail.com>
2022-04-22 08:34:42 +0900

    회원가입

--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -3,28 +3,52 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import {FormControl, InputLabel, MenuItem, NativeSelect, Select} from "@mui/material";
-import {useEffect} from "react";
+import axios from "axios";
+import {useEffect, useState} from "react";
+
 
 
 
 function SignUp() {
-    const handleSubmit = (event) => {
+    const [userEmail,setUserEmail] = useState('');
+    const [userPassword,setUserPassword] = useState('');
+    const [userNickname,setUserNickname] = useState('');
+    const [userMbti, setUserMbti] = React.useState('');
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
+        const baseurl = process.env.REACT_APP_BASE_URL;
+        axios.post(baseurl + '/api/v1/users/create/',
+            {
+                email: userEmail,
+                password: userPassword,
+                nickname: userNickname,
+                mbti: userMbti
+            }
+        )
+            .then(function (response) {
+                alert('회원가입 성공');
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     };
 
-    const [userMbti, setUserMbti] = React.useState('');
+    const emailChange = (event) => {
+        setUserEmail(event.target.value);
+    }
+    const passwordChange = (event) => {
+        setUserPassword(event.target.value);
+    }
+    const nicknameChange = (event) => {
+        setUserNickname(event.target.value);
+    }
     const handleChange = (event) => {
         setUserMbti(event.target.value);
     };
@@ -39,6 +63,7 @@ function SignUp() {
             )
             i+=1;
         }
+
     return (
 
             <Container component="main" maxWidth="xs">
@@ -63,10 +88,25 @@ function SignUp() {
                                 <TextField
                                     required
                                     fullWidth
-                                    id="name"
-                                    label="이름"
-                                    name="name"
-                                    autoComplete="name"
+                                    id="email"
+                                    label="이메일"
+                                    name="email"
+                                    value={userEmail}
+                                    autoComplete="email"
+                                    onChange={emailChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="비밀번호"
+                                    value={userPassword}
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                    onChange={passwordChange}
                                 />
                             </Grid>
                             <Grid item xs={12} sm={6}>
@@ -92,30 +132,11 @@ function SignUp() {
                                     required
                                     fullWidth
                                     id="nick"
+                                    value={userNickname}
                                     label="닉네임"
                                     name="nick"
                                     autoComplete="family-name"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="이메일"
-                                    name="email"
-                                    autoComplete="email"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="비밀번호"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="new-password"
+                                    onChange={nicknameChange}
                                 />
                             </Grid>
                         </Grid>
