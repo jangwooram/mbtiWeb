0dbd580bb1ee3a756ef3edfb5329bf1c0a0de914
taehyun kim <jamesk3121@gmail.com>
2023-07-20 17:25:30 +0900

    ADD: init file tree

---
+++ src/component/Argument/Hot.js
@@ -0,0 +1,16 @@
+import React, {useEffect} from "react";
+import {Link} from "react-router-dom";
+
+function ArguementHot(props) {
+    /**
+     * 핫한 논쟁 컴포넌트
+     */
+
+    return (
+        <div className="contents">
+            <span>~핫한 논쟁~</span>
+        </div>
+    );
+}
+
+export default ArguementHot;
---
+++ src/component/Home/Content.js
@@ -0,0 +1,21 @@
+import React, {useEffect} from "react";
+import ArguementHot from "../Argument/Hot";
+import BoardHot from "../Board/Hot";
+import BoardTagHot from "../Board/TagHot";
+import {Link} from "react-router-dom";
+
+function HomeContent(props) {
+    /**
+     * 홈페이지 대문 컴포넌트
+     */
+
+    return (
+        <div className="contents">
+            <ArguementHot/>
+            <BoardHot/>
+            <BoardTagHot/>
+        </div>
+    );
+}
+
+export default HomeContent;
